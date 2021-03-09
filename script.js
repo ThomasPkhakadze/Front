ca0d2cd6aa26089aa40ebd8c6da2a2cb572daa5c
ThomasPkhakadze <thompkhakadze@gmail.com>
2021-03-09 22:50:44 +0400

    git commit

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
   $('.nav-btn').click(function () {
-    var navWidth = $('.menu-nav').animate({ width: 'toggle' }, 350);
+    $('.menu-nav').css('visibility', 'visible');
+    $('.menu-nav').animate({ width: 'toggle' }, 400);
+    $('.nav-ul li').mouseover(function () {
+      console.log('success');
+
+      $('.menu-nav-2').css('visibility', 'visible');
+    });
   });
 });
