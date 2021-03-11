87364b60a97ed2193d35d40e18d1100316933ef9
ThomasPkhakadze <thompkhakadze@gmail.com>
2021-03-11 23:52:11 +0400

    git commit

--- script.js
+++ script.js
@@ -1,11 +1,59 @@
 $(document).ready(function () {
-  $('.nav-btn').click(function () {
-    $('.menu-nav').css('visibility', 'visible');
-    $('.menu-nav').animate({ width: 'toggle' }, 400);
-    $('.nav-ul li').mouseover(function () {
-      console.log('success');
-
-      $('.menu-nav-2').css('visibility', 'visible');
-    });
+  $('.menu-btn').click(function () {
+    $('.down').animate({ top: '8px', opacity: '0' }, 300);
+    $('.up').animate({ top: '8px', opacity: '0' }, 300);
+    $('.rotateL').animate(
+      {
+        borderSpacing: 45,
+      },
+      {
+        step: function (now) {
+          $(this).css('transform', 'rotate(' + now + 'deg)');
+        },
+      },
+      1000
+    );
+    $('.rotateR').animate(
+      {
+        borderSpacing: -45,
+      },
+      {
+        step: function (now) {
+          $(this).css('transform', 'rotate(' + now + 'deg)');
+        },
+      },
+      1000
+    );
+    $('.menu-btn').addClass('clicked');
   });
+
+  if ($('.menu-btn').has('.clicked')) {
+    $('.menu-btn').click(function () {
+      // $('.down').animate({ top: '0px', opacity: '1' }, 300);
+      // $('.up').animate({ top: '16px', opacity: '1' }, 300);
+      // $('.rotateL').animate(
+      //   {
+      //     borderSpacing: 0,
+      //   },
+      //   {
+      //     step: function (now) {
+      //       $(this).css('transform', 'rotate(' + now + 'deg)');
+      //     },
+      //   },
+      //   1000
+      // );
+      // $('.rotateR').animate(
+      //   {
+      //     borderSpacing: 0,
+      //   },
+      //   {
+      //     step: function (now) {
+      //       $(this).css('transform', 'rotate(' + now + 'deg)');
+      //     },
+      //   },
+      //   1000
+      // );
+      $('.menu-btn').removeClass('clicked');
+    });
+  }
 });
