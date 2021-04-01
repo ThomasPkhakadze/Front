e728e9bfddd9404ec10e0223ef74c5940e3c584a
ThomasPkhakadze <thompkhakadze@gmail.com>
2021-04-01 21:24:23 +0400

    git commit

--- script.js
+++ script.js
@@ -68,4 +68,45 @@ $(document).ready(function () {
     $(this).removeClass('active');
     $('.sub-menu').fadeOut(300);
   });
+
+  // on hover add effect
+  $('.rectangle').hover(
+    function () {
+      $(this).children(':first-child').addClass('fromLeft');
+      $(this).children(':first-child').animate({ right: '0px' }, 800);
+    },
+    function () {
+      $(this).children(':first-child').animate({ right: '350px' }, 500);
+      setTimeout(function () {
+        $(this).children(':first-child').removeClass('fromLeft');
+      }, 1000);
+    }
+  );
+  // on click change slide
+
+  // making first slide & rectangle appear and active on default
+  $('.slideshow-container').children(':first-child').show();
+
+  //on click not active rectangle
+  $('.rectangle:not(.active)').click(function () {
+    $('.mySlides').css('zIndex', '1');
+    // remove rectangle active class
+    $('.rectangle.active').removeClass('active');
+    // capture id of an rectangle element
+    var id = $(this).attr('id');
+    // show slide with the same id
+    $('.mySlides#' + id).fadeIn(1700);
+    // make active slide bellow the upcoming slide
+    $('.mySlides#' + id).css('zIndex', '2');
+
+    // setting timeout to maintain previus slide as a background
+    setTimeout(function () {
+      // then after next slide arrives hiding and removing/adding active classes
+      $('.mySlides.active').hide();
+      $('.mySlides.active').removeClass('active');
+
+      $('.rectangle#' + id).addClass('active');
+      $('.mySlides#' + id).addClass('active');
+    }, 1700);
+  });
 });
